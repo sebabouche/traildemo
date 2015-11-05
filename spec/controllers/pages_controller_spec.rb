e28f659d9a7b5240a0bf0eabbfe149c8d57edc70
sebabouche <s.nicolaidis@me.com>
2015-11-05 09:27:08 +0100

    Add swp and swo to gitignore and remove tracking of them

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
   context "GET home" do
     
-    it "reads like a sentence (almost)" do
+    it "renders the home page" do
       get :home
       expect(response).to be_success
       expect(response).to render_template(:home)
