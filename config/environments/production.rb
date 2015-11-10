5259d3f05d3331d281b77d5beb9ac12f75d825e6
sebabouche <s.nicolaidis@me.com>
2015-11-10 18:47:47 +0100

    modified asset host env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
 
   config.log_level = :debug
 
-  config.action_controller.asset_host = ENV['CDN_ASSET_HOST']
+  config.action_controller.asset_host = ENV['ASSET_HOST']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
